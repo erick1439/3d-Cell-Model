6b3db16f25a47432b0595c0c733741911b0b8520
Erick Pena <erick1439@live.com>
2020-09-08 13:09:09 -0400

    heroku setting

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.use(express.static(__dirname + '/public'))
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
-app.listen(3000, () =>
-  console.log('Visit http://127.0.0.1:3000')
+app.listen(process.env.PORT || 3000, () =>
+  console.log('running...')
 );
